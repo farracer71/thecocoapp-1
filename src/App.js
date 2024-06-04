1170811d53a8fb6b1e3292f79a5239939ca9bf6c
PropsCoder-Raj <panditrohit532@gmail.com>
2024-06-04 23:22:45 +0530

    Uninstall Firebase.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,7 @@ import React from "react";
 import {  CssBaseline } from "@mui/material";
 import AppRouter from "./AppRouter";
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAw2IblpOvbNeFmGu8YQy4RpITB2Z7TH3c",
-//   authDomain: "thecocoapp.firebaseapp.com",
-//   projectId: "thecocoapp",
-//   storageBucket: "thecocoapp.appspot.com",
-//   messagingSenderId: "308584814158",
-//   appId: "1:308584814158:web:28f73e4576592d081a151f",
-//   measurementId: "G-KDNMVRYTYS"
-// };
-
 function App() {
-
-  // Initialize Firebase
-  // const app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
-
   return (
     <div className="App">
       <div>

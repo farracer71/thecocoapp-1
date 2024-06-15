383e109d34b0b7c2d07b3cfad1b3b6d442885871
RohitSIT <rohit@spareit.co.in>
2024-06-15 19:45:46 +0530

    Update Code.

--- backend/src/api/enums/userType.js
+++
@@ -1,7 +0,0 @@
-// Export an object with user types as properties
-module.exports = Object.freeze({
-    // Constant representing an admin user type
-    ADMIN: 'ADMIN',
-    // Constant representing a regular user type
-    USER: 'USER'
-});

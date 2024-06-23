660e12159f1267650ce978a1427eabc190cea08b
RohitSIT <rohit@spareit.co.in>
2024-06-23 12:41:39 +0530

    Integrate Auto Default Questions Add.

--- backend/src/api/config/db.js
+++ backend/src/api/config/db.js
@@ -6,12 +6,14 @@ const Standards = require('../model/Standards');
 const Modules = require('../model/Modules');
 const Levels = require('../model/Levels');
 const Lessons = require('../model/Lessons');
+const Questions = require('../model/Questions');
 
 let initialSchools = require('../data/schools');
 let initialStandards = require('../data/standards');
 let initialModules = require('../data/modules.json');
 let initialLevels = require('../data/levels.json');
 let initialLessons = require('../data/lessons.json');
+let initialQuestions = require('../data/questions.json');
 
 // MongoDB connection URI
 const mongoURI = process.env.mongodb_url;
@@ -135,6 +137,41 @@ const importInitialLessons = async () => {
     }
 };
 
+const importInitialQuestions = async () => {
+    try {
+        const count = await Questions.countDocuments();
+        if (count === 0) {
+            const modulesList = await Modules.find();
+            const standardsList = await Standards.find();
+            const levelsList = await Levels.find();
+            const updatedLevels = initialQuestions.map((element) => {
+                const standard = standardsList.find((standard) => standard.standard_id == element.standard_id);
+                if(standard){
+                    const modules = modulesList.find((module) => module.module_id == element.module_id && module.standard_id.toString() == standard._id.toString());
+                    if (modules) {
+                        const levels = levelsList.find((levels) => levels.level_id == element.level_id && levels.standard_id.toString() == standard._id.toString() && levels.module_id.toString() == modules._id.toString());
+                        if (levels) {
+                            return {
+                                ...element,
+                                level_id: levels._id,
+                                module_id: levels.module_id,
+                                standard_id: levels.standard_id // Use ObjectId directly
+                            };
+                        }
+                    }
+                }
+            });
+
+            await Questions.insertMany(updatedLevels);
+            console.log('Initial Questions data imported successfully.');
+        } else {
+            console.log('Questions collection is not empty, skipping initial data import.');
+        }
+    } catch (error) {
+        console.error('Error importing initial school data:', error);
+    }
+};
+
 // Connect to MongoDB
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
@@ -144,6 +181,7 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
         await importInitialModules();
         await importInitialLevels();
         await importInitialLessons();
+        await importInitialQuestions();
     })
     .catch((error) => {
         console.error('MongoDB connection error:', error);
